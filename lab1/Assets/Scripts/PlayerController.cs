38719b60e55ad84ca1353cf8ea5ac126fc545eb6
Benjamin Charlton <kuldrgames@gmail.com>
2019-05-01 17:23:38 +0100

    Lab1 - Up to Firing

---
+++ lab1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float xMin = -5.0f;
+    public float xMax = 5.0f;
+    public float zMin = -5.0f;
+    public float zMax = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Debug.Log("Input: " + horizontalMovement + " " + verticalMovement);
+
+        Rigidbody r = GetComponent<Rigidbody>();
+
+        r.velocity = new Vector3(
+            horizontalMovement * speed,
+            0.0f,
+            verticalMovement * speed);
+        r.position = new Vector3(
+            Mathf.Clamp(r.position.x, xMin, xMax),
+            r.position.y,
+            Mathf.Clamp(r.position.z, zMin, zMax));
+    }
+}
