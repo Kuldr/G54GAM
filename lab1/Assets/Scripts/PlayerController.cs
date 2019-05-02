f77b1e6ee1a835a907d76746f8604f2823f0985b
Benjamin Charlton <kuldrgames@gmail.com>
2019-05-02 19:46:39 +0100

    lab1 - extension code but buggy

--- lab1/Assets/Scripts/PlayerController.cs
+++ lab1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -49,5 +50,11 @@ public class PlayerController : MonoBehaviour
                 shotTransform.position,
                 shotTransform.rotation);
         }
+    }
+
+    private void OnDestroy()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
